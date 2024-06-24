fecc37dd18857fb9b0f1a8112cb2fdc97ab6dcad
Muhammad Rizwan Aslam <letsmailriz@gmail.com>
2024-06-24 20:27:04 +0200

    Updated the value of K in similarity search

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ if __name__ == "__main__":
 
         uploaded_file = st.file_uploader('Upload a file: ', type=['pdf', 'docx', 'txt'])
         chunk_size = st.number_input('Chunk size:', min_value=100, max_value=2048, value=512, on_change=clear_history)
-        k = st.number_input('k', min_value=1, max_value=20, value=3, on_change=clear_history)
+        k = st.number_input('k', min_value=5, max_value=20, value=6, on_change=clear_history)
         add_data = st.button('Upload File', on_click=clear_history)
 
         if uploaded_file and add_data:
